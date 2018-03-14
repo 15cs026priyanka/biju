fe2322658ef9eee5fd18689d92b1749371e03f29
15cs026priyanka <priyankaappu1998@gmail.com>
2018-03-14 16:12:02 +0530

    Create input.py

---
+++ input.py
@@ -0,0 +1,10 @@
+user_input = -1
+_continue = True
+while _continue:
+    try:
+        user_input = int(raw_input("Value: "))
+        if 1111 <= user_input  <= 9999:
+            _continue = False
+   else:
+      _continue=true
+      pass the values
